592b3f787753f2b5d6eb8b231af1d377261f5310
Rygel-XVI <micha232@umn.edu>
2019-10-19 09:41:27 -0700

    updated Channel#update_if_needed and working on #update_videos and the get request

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,14 +2,38 @@ class Channel < ApplicationRecord
   has_many :videos
 
   def self.update_if_needed
-    channels = Channel.all
+
+    # in the future if there is more than one channel will make it a string and convert it into an array of id's to manipulate
+    # binding.pry
+    channels = [ENV['CHANNEL_ID']]
 
     channels.each do |channel|
-      if (Time.now.utc - channel.updated_at > 61200)
+
+      # find or create the channel
+      c = Channel.find_or_create_by(channel_id: channel)
+
+      # if it has not been updated in 24 hrs or is newly created
+      if ((c.created_at === c.updated_at) || (Time.now.utc - c.updated_at > 61200))
+
         # do request to update this channel and videos
+        c.update_videos
       end
     end
 
   end
 
+  def update_videos
+    cid = self.channel_id
+    channel = HTTP.get("https://www.googleapis.com/youtube/v3/search?key=#{ENV["TEST_API"]}&channelId=#{cid}&part=snippet,id&type=video&maxResults=50")
+
+    binding.pry
+
+    # channel.parse gives json
+  end
+
+
 end
+
+
+# HTTP.get("https://github.com").body
+# look into async calls ruby rails em-http-request
