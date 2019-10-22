c98caf5bd6abb6c32727a11423300b7b4ae8e196
Rygel-XVI <micha232@umn.edu>
2019-10-21 18:50:54 -0700

    created video_id string and get request for it

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,24 @@ class Video < ApplicationRecord
 
 
   def self.update_by_id(video_ids)
+
+    video_ids_string = video_ids.join(",")
+
+    videos = HTTP.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=#{video_ids_string}&key=#{ENV["TEST_API"]}").parse
     binding.pry
+
+    videos["items"]
+    # produces array of nil or video objects
+    # videos = video_ids.map do |id|
+    #   Video.find_by(embed_id: id)
+    # end
+    #
+    # videos.each do |video|
+    #   if (video == nil)
+    #
+    #   end
+    # end
+
   end
 
 end
