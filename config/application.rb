1c716f64a42a9adee3098bf3237c25d397ecb4dd
Rygel-XVI <micha232@umn.edu>
2019-10-28 20:13:04 -0700

    added rack-cors

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,14 @@ Bundler.require(*Rails.groups)
 module SeeSignStoriesRailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # config.load_defaults 6.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
