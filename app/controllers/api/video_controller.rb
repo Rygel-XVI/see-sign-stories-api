a30a8cacdce33181cb99cceeb1ecf1df310c42e0
Rygel-XVI <micha232@umn.edu>
2019-10-29 19:32:17 -0700

    remaking with new interface for ease of use for the teachers. making it more RESTful and adding routes. note to self look into free SSL

--- app/controllers/api/video_controller.rb
+++ app/controllers/api/video_controller.rb
@@ -8,8 +8,28 @@ class Api::VideoController < ApplicationController
     render json: @videos
   end
 
+  def new
+    @video = Video.new(video_params)
+    @video.save
+    redirect_to api_video_index_path
+  end
+
+  def update
+    binding.pry
+  end
+
+  def destroy
+
+  end
+
   # def update
   #   Video.update_all
   # end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :description, :embed_id, :ar_lvl_high, :ar_lvl_low, :grade)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   root :to => "api/video#index"
 
   namespace :api do
-      get "video/index", to: "video#index"
-      get "channel/index", to: "channel#index"
-      put "channel/update", to: "channel#update"
+    resources :video, :channel, only: [:index, :new, :update, :destroy]
+
+      # get "video/index", to: "video#index"
+      # get "channel/index", to: "channel#index"
+      # put "channel/update", to: "channel#update"
 
   end
 
