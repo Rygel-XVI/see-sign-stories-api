988fef7ac0af189d4e05c9cd29f4a4b7f099b031
Rygel-XVI <micha232@umn.edu>
2020-03-22 21:34:04 -0700

    removed all routes but home since current setup only needs one spot

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root :to => "api/video#index"
+  root :to => "channel#index"
 
-  namespace :api do
-      get "video/index", to: "video#index"
-      get "channel/index", to: "channel#index"
-      put "channel/update", to: "channel#update"
+  # namespace :api do
+      # get "video/index", to: "video#index"
+      # get "channel/index", to: "channel#index"
+      # put "channel/update", to: "channel#update"
 
-  end
+  # end
 
 end
