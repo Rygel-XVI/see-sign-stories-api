f0e1a30aa601045b07588578a87b8ed4c846abdd
Rygel-XVI <micha232@umn.edu>
2020-03-22 21:36:30 -0700

    added api request to youtube and saving into db

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -22,65 +22,71 @@ class Channel < ApplicationRecord
   #
   # end
   
-  def self.update_channel
-    videos = HTTP.get(`https://www.googleapis.com/youtube/v3/search?key=#{TEST_API}&channelId=#{CHANNEL_ID}&part=snippet,id&type=video&maxResults=50`).parse
+  def self.update_channel    
+    url = "https://www.googleapis.com/youtube/v3/search?key=#{ENV["TEST_API"]}&channelId=#{ENV["CHANNEL_ID"]}&part=snippet,id&type=video&maxResults=50"
     
-    ## setup postgres on computer
-    binding.pry
+    response = HTTP.get(url).parse
+    
+    parsed_response = parse_response(response['items'])
   end
+  
+  def self.parse_response(response)
+    videos = []
+    
+    response.each do |video|
 
+      embed_id = video.dig('id','videoId')
+      tags = fetch_video_tags(embed_id)
 
-  def self.channels_to_update
-    Channel.check_for_new_channels
-    Channel.all.each do |channel|
-      channel.update_channel if channel.update_needed?
-    end
-    # Channel.all.filter{|c| c.update_neefded? }
-  end
-
-# returns array of channels
-  def self.check_for_new_channels
-    channel_ids = Channel.get_and_format_channel_ids
-    channel_ids.map do |cid|
-      channel = Channel.find_by(channel_id: cid)
-
-      if (channel == nil)
-        c = Channel.create(channel_id: cid)
+      video_attributes = {
+        title: tags[:title],
+        description: tags[:description],
+        embed_id: embed_id,
+        ar_lvl_high: tags[:ar_lvl_high],
+        ar_lvl_low: tags[:ar_lvl_low],
+        grade: tags[:grade]
+      }
+      
+      vid = Video.find_by(embed_id: embed_id)
+      
+      if vid
+        vid.update(video_attributes)
+      else
+        Video.create(video_attributes)
       end
+      
+    end
+    
+  end
+  
+  def self.fetch_video_tags(embed_id)
+    url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=#{embed_id}&key=#{ENV["TEST_API"]}"
+    
+    response = HTTP.get(url).parse['items'][0]['snippet']
 
+    ar_lvl_low, ar_lvl_high = parse_ar_lvls(response["tags"])
+    
+    grade = response['tags']&.find{|tag| tag.match(/^Grade/)}
+
+    {
+      ar_lvl_high: ar_lvl_high,
+      ar_lvl_low: ar_lvl_low,
+      grade: grade,
+      title: response['title'],
+      description: response['description']
+    }
+    
+  end
+  
+  
+  def self.parse_ar_lvls(tags)
+    ar_lvls = tags&.find{|tag| tag.match(/^AR/)}&.split(/\s|-/)
+    if ar_lvls.is_a?(Array) && ar_lvls.size >= 3
+      [ ar_lvls[1].to_f, ar_lvls[2].to_f ]
+    else
+      [ nil, nil ]
     end
   end
-
-  # if there are more channel ids in the future we will parse them here
-  def self.get_and_format_channel_ids
-    [ENV['CHANNEL_ID']]
-  end
-
-# currently updates once every 24hrs or after creation
-  def update_needed?
-    self.updated_at == self.created_at || Time.now.utc - self.updated_at > 61200
-  end
-
-
-
-# this should do
-#   => api request to pull the video ids from the channel
-#   => api request to pull the video information from the video ids
-  # => return the info to the controller to update utilizing params
-
-  def update_channel
-    cid = self.channel_id
-    channel = HTTP.get("https://www.googleapis.com/youtube/v3/search?key=#{ENV["TEST_API"]}&channelId=#{cid}&part=snippet,id&type=video&maxResults=50").parse
-
-    videos = channel["items"]
-
-    video_ids = videos.map do |video|
-      video["id"]["videoId"]
-    end
-
-    Video.update_by_id(video_ids)
-  end
-
 
 end
 
