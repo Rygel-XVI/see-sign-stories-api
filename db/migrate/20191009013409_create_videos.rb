dfbc927adc57f9939eed101908f694464a95e3e8
Rygel-XVI <micha232@umn.edu>
2020-04-19 12:45:42 -0700

    removed unusedtables, redid datamodel logic

--- db/migrate/20191009013409_create_videos.rb
+++ db/migrate/20191009013409_create_videos.rb
@@ -1,12 +1,16 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
     create_table :videos do |t|
-      t.string :title
-      t.string :description
-      t.string :embed_id
-      t.float :ar_lvl_high
-      t.float :ar_lvl_low
-      t.string :grade
+      t.string "embed_id"
+      t.string "title"
+      t.string "description"
+      t.float "ar_lvl_high"
+      t.float "ar_lvl_low"
+      t.string "grade"
+      t.boolean "is_chapter_book"
+      t.integer "chapter"
+      
+      t.string "tags"
 
       t.timestamps
     end
--- db/migrate/20191009020305_create_video_genres.rb
+++
@@ -1,8 +0,0 @@
-class CreateVideoGenres < ActiveRecord::Migration[6.0]
-  def change
-    create_table :video_genres do |t|
-
-      t.timestamps
-    end
-  end
-end
